949fe2b39a816b76b920bf646c14774076cfe857
Samson Githinji <githinjisamson148@gmail.com>
2023-10-27 07:40:52 +0300

    Completed assignment

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -9,8 +9,33 @@ function SpicyFoodList() {
     console.log(newFood);
   }
 
+  // !remove element from arrays in state/based on id
+  const handleRemoveFood = (id) => {
+    return foods.filter((food) => {
+      return food.id !== id;
+    });
+  };
+
+  // !update elements in arrays in state/based on id
+  const handleUpdateFood = (id) => {
+    return foods.map((food) => {
+      if (food.id === id) {
+        return { ...food, heatLevel: food.heatLevel + 1 };
+      } else {
+        return food;
+      }
+    });
+  };
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li
+      key={food.id}
+      // onClick={() => {
+      //   handleRemoveFood(food.id);
+      // }}
+      onClick={() => {
+        handleUpdateFood(food.id);
+      }}
+    >
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
@@ -24,3 +49,30 @@ function SpicyFoodList() {
 }
 
 export default SpicyFoodList;
+
+// TODO: array cheatsheet
+// adding: [...]/spread operator
+// update: .map()
+// remove: .filter()
+
+// can work with multiple state variables
+
+// states
+// const [foods, setFoods] = useState(spicyFoods);
+// const [filterBy, setFilterBy] = useState("All");
+
+// shortened array
+// const foodsToDisplay = foods.filter((food) => {
+//   if (filterBy === "All") {
+//     return true;
+//   } else {
+//     return food.cuisine === filterBy;
+//   }
+// });
+
+// iterating over shortened array
+// const foodList = foodsToDisplay.map((food) => (
+//   <li key={food.id} onClick={() => handleLiClick(food.id)}>
+//     {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+//   </li>
+// ));
